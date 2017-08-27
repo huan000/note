cacaa21dc857baeac5c9525d8dd37ce340ba5b01
huan0000 <huan@admin.com>
2017-08-27 15:40:57 +0800

    centos6.5/1-50课.php

---
+++ test/centos6.5/100-150.php
@@ -0,0 +1,350 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huan
+ * Date: 2017/6/24
+ * Time: 19:43
+ */
+
+
+/**
+ *  重要的子目录
+ *   /etc
+ *     网卡配置文件：
+ *     /etc/sysconfig/network-scripts/ifcfg-eth0             网卡配置文件
+ *     /etc/init.d/network restart                           重启所有网卡
+ *     ifdown eth0 &&  ifup eth0                             重启个别网卡
+ *     setup                                                 图形化设置网卡
+ *     route -n                                              查看网关
+ *
+ *
+ *     配置项目：
+ *     DEVICE=eth0               物理设备名称
+ *     HWADDR=...                网卡的mac地址
+ *     TYPE=Ethernet             类型是以太网卡
+ *     UUID=....                 网卡的唯一标识
+ *     ONBOOT=yes                开机是否自动启动
+ *     BOOTPROTO=none            none 引导时不适用协议 || static 使用静态ip || bootp 使用这种协议 || dhcp  使用此协议
+ *     IPADDR=127.0.0.1          设置ip地址
+ *     NETMASK=255.255.255.0     设置子网掩码
+ *     DNS2=202.22.22.22         dns地址  就是域名和ip的解析工具
+ *     GETWAY=10.10.10           网关
+ *
+ *
+ *     本地dns配置文件
+ *     /etc/resolv.conf          本地dns配置文件
+ *     网卡中配置的dns 会优先于 resolv.conf    如果重启网络服务，则会覆盖掉resolv.conf
+ *
+ *     hosts 配置文件
+ *     /etc/hosts
+ *
+ *     配置主机名的目录
+ *     /etc/sysconfig/network
+ *
+ *     开机自动挂载的文件系统
+ *     /etc/fstab
+ *
+ *     开机自启动软件
+ *     /etc/rc.local
+ *     ps:用于存放开机自启动程序命令的文件（chkconfig常用来管理yum/rpm安装的程序的开机自启动）
+ *        自己开发的程序一般把启动命令放入rc.local 实现开机自启动
+ *
+ *     开机执行运行级别
+ *     /etc/inittab
+ *
+ *     通过yum或rpm工具安装的软件的默认启动程序目录
+ *     /etc/init.d  =>  /etc/rc.d/init.d          类似于防火墙，ssh等等
+ *
+ *     系统全局环境变量
+ *     /etc/profile
+ *
+ *     用户登陆的时候可以加载一些程序和脚本
+ *     /etc/profile.d
+ *
+ *     登陆时候的提示
+ *     /etc/issue
+ *
+ *     登陆后的提示
+ *     /etc/motd
+ *
+ *     显示版本的配置文件
+ *     /etc/redhat-release
+ *
+ *     设定用户的组名和相关信息
+ *     /etc/group
+ *
+ *     账号信息文件
+ *     /etc/passwd
+ *
+ *     账号密码文件
+ *     /etc/shadow
+ *
+ *     组密码文件
+ *     /etc/gshadow
+ *
+ *     可以执行sudu命令的配置文件
+ *     /etc/sudoers
+ *
+ *     设定哪些终端可以登陆root
+ *     /etc/securetty
+ *
+ *     日志配置文件
+ *     /etc/syslog.conf       日志设置文件   centos 5.x
+ *     /etc/syslog.conf       日志设置文件   centos 6.x
+ *
+ *
+ *     /usr  目录
+ *        /usr/local                这个目录一般存放用户源代码安装的软件
+ *        /usr/src                  这个目录存放用户的源码包
+ *        /usr/local/bin            用户安装的程序和/usr/local目录下应用的符号链接
+ *
+ *
+ *
+ *     /var   目录
+ *        /var/log                  系统日志的存放地址
+ *        /var/log/messages         系统信息的默认日志文件。
+ *        /var/log/secure           纪录登入系统存取信息的文件
+ *        /var/spool                定时任务的路径
+ *        /var/spool/cron/root
+ *        /var/spool/mail           系统用户邮件存放目录
+ *        /var/spool/clientmqueue   sendmail临时邮件文件目录  centos 5
+ *
+ *
+ *    /proc
+ *        /proc/version             内核版本
+ *        /proc/sys/kernel          系统内核功能
+ *        /proc/mounts              系统挂载的信息
+ *        /proc/loadavg             系统挂载的平均值
+ *        /proc/ioports             正在使用的io端口
+ *        /proc/devices             当前内核运行配置的所有设备清单
+ *
+ *
+ *
+ *    考试题：
+ *        linux命令行下使用反斜杠“\”调用别名的原命令
+ *        xargs -n 1
+ *        /etc/profile     /etc/bashrc    ~。/bashrc
+ *
+ *        如何查找目录
+ *        find -maxdepth 1 -type d
+ *        ls -F | grep (*\/)       过滤目录带斜线的
+ *        ls -lrt                  r代表倒叙  t代表按照修改时间排序
+ *        ls -a                    显示隐藏文件
+ *        ls -lh  || ll-h          查看文件大小
+ *        ll -d                    显示目录时候必须加d
+ *        ll -i                    显示文件的inode号
+ *
+ *        复制文件或者目录
+ *        cp -a                    复制文件保持文件属性不变(  a=pdr r代表可以复制目录)
+ *
+ *        查看这个是目录还是文件
+ *        file  dir
+ *
+ *        从标准输入获取数据并将数据换成命令行参数
+ *        标准输入的来历 ：  管道或stdin ， 输入重定向
+ *        xargs  -n    指定每一行最多的个数
+ *        xargs -i {}   配合 {}来使用注入参数
+ *
+ *        树形打印
+ *        tree -Ld 1      -L 显示层数  -d 显示目录
+ *
+ *
+ *
+ *        grep --color=auto        加上颜色乐乐
+ *
+ *        find -atime n           n天之前被access过的档案
+ *        find -ctime n           n天之前被change过的档案
+ *        find -mtime n           n天之前杯modife过的档案
+ *
+ *        tail -f                 动态查看一个文件的最后几行
+ *        tailf   和 tail -f  一样
+ *
+ *        cat -n                  显示行号
+ *        nl  file                显示行号  （空行不记录行号)
+ *        less -N                 显示行号
+ *        wc -l                   显示总行数
+ *
+ *
+ *     设置开机自启动
+ *        第一种配置方法： (方法一会覆盖方法二)
+ *            可以把要启动的服务命令放在 /etc/rc.local 里面
+ *            例子：  vi /etc/rc.local
+ *                    /etc/init.d start             加上这一行就可以开机自启动
+ *
+ *        第二种配置方法：  (控制下次启动)
+ *            chkconfig --list                      查看所有程序在不同运行级别的启动开启于否
+ *            chkconfig sshd off                    默认关闭 run level 2345  off
+ *            chkconfig --level 23  off             关闭23级别下面的chkconfig
+ *
+ *        chkconfig  原理 ：
+ *            /etc/rc.sysinit                       系统初始化运行脚本
+ *            /etc/rc.d                             所有运行级别执行的脚本
+ *            执行chkconfig 回向  /etc/rc.d/rc3.d    写入指定命令
+ *
+ *
+ *        使自己的脚本可以通过chkconfig来进行管理
+ *            1. 首先将启动程序 selfd 放入/etc/rc.d/init.d
+ *            2. 编写开头文件
+ *               # chkconfig： 35 24 25           35运行级别    24启动位置   关闭位置
+ *               # description：  描述
+ *            3. chkconfig --add selfd            加载进chkconfig
+ *
+ *
+ *
+ *     课126： linux 启动过程
+ *        1.   开机bios 自检
+ *        2.   mbr引导   0柱面 0磁道 1扇区 前446字节的grub菜单     一共512字节
+ *        3.   grub 引导菜单   /etc/grub.conf
+ *        4.   加载内核  kernel
+ *        5.   启动第一个启动进程   init
+ *        6.   读取文件
+ *                 /etc/inittab                     启动级别的文件
+ *                 /etc/rc.d/sysinit                初始化系统   (设置主机名，fstab自动挂载等等)
+ *                 /etc/rc.d/rcx.d                  执行这个运行级别下面的脚本，启动该级别的程序
+ *        7.   启动tty                               启动远程链接
+ *
+ *
+ *      课128： 刚开始必须有的程序  (关掉其余没用的应用 开启这几个应用)
+ *        sshd              远程链接
+ *        rsyslog           日志收集
+ *        notwork           网络服务
+ *        crond             定时脚本
+ *        sysstat           查看性能的服务
+ *
+ *      chkconfig --list | grep "3:on"               查看3级别上所有运行的服务
+ *
+ *      grep -E "123|234"  text.txt                  同时过滤两个字符串
+ *      egrep   "123|234"  text.txt                  同时过滤两个字符串
+ *
+ *
+ *      课136：文件属性描述
+ *      是一个磁盘可以存放数据经历下面的步骤：
+ *      分区-》 格式化 -》存放数据
+ *      格式化：  创建文件系统 ，一般过程是两个部分，第一部分是生成inode ， 第二部分是生成block
+ *      block是用来存放实际数据的，
+ *      inode是用来存放这些属性信息的 (也就是ls -l的结果)，还包含和block的关联信息 ，但是
+ *      唯独不包括文件名字
+ *
+ *      dumpe2fs /dev/sda1 |grep -i "inode size"            查看grub分区的大小
+ *      dumpe2fs /dev/sda1 |grep -i "inode count"  block    查看inode的总大小
+ *
+ *      df -i        查看inode 分配情况
+ *
+ *      inode 在全区是唯一的，除非硬链接的情况，硬链接相当与进入一个窗户的两个门
+ *
+ *      centos 5  inode 是125k      centos6 inode 是256k
+ *      一个block  一般是1-4k   通常是4k
+ *
+ *      如果改变inode的大小 一般不会这么做  在格式化的时候指定
+ *
+ *
+ *
+ *
+ *      138课：
+ *      block 知识小结
+ *      1. 一个文件可能占用多个block，但是每读取一次block就会io开销一次
+ *      2. 如果要提升io开销，就要一次尽可能的多读取一些数据
+ *      3. 一个block只能放一个文件的内容 ，无论文件多小，如果block是4k，存放一个1k的文件剩余3k就浪费了
+ *      4. block根据需求灵活设置大小
+ *      5. block的大小设置也是格式化的时候设置的
+ *      6. 文件名不再inode 里  在上级目录的block里面
+ *
+ *      mkfs.ext4 -b2014 -i256 /dev/sdb        -b 是block大小  -i是inode大小
+ *
+ *
+ *      139课： 文件类型
+ *      file xxx.txt      通过这个命令来查看
+ *
+ *      d    目录
+ *      -    普通文件
+ *      l    符号链接文件
+ *      b    表示块设备或者其他外围设备  (磁盘或者光驱)
+ *      c    表示字符设备文件   (猫或者串口设备)
+ *      s    表示socket文件
+ *      p    表示管道文件
+ *
+ *      - 普通文件
+ *      1. 纯文本文件        ascII     可以正常cat等读取
+ *      2. 二进制文件        binary    比如/bin/ls   文件
+ *      3. 数据格式文件      data      不如lastlog   他的文件  /var/log/lastlog  (存放某些命令的信息) 汇报所有用户的登陆情况
+ *                                                     /var/log/wtmp (last命令的数据文件)  查看用户登陆信息
+ *
+ *
+ *     142课：
+ *        r  4   w  2  x 1
+ *        rwx   读写执行
+ *
+ *     143课：
+ *        ln  创建硬链接
+ *        ln -s   创建软连接
+ *
+ *     硬链接知识小节：
+ *        1. 文件的inode节点号相同
+ *        2. 删除一个文件 。文件实体并未被删除，只删除了文件的链接
+ *        3. 只有删除了源文件和所有对应的硬链接文件，文件实体才会被删除
+ *        4. 硬链接也是普通文件
+ *        5. 对与静态文件 (没有进程调用的文件)  如果硬链接的数量为0  则该文件就被删除了  ls -l 的第三列
+ *        6. 不可以对目录进行硬链接
+ *        7. 删除链接文件的源文件，对硬链接文件没有影响
+ *        8. 硬链接不能跨目录
+ *
+ *     软连接知识小节：
+ *        1. 属于两个文件  inode 不一样
+ *        2. 文件的类型不一样 ，一个是- 一个是l
+ *        3. 文件的属性也不一样
+ *        4. 改变软连接文件的属性实际上更改了源文件
+ *        5. 软链接文件包含的路径指向了源文件的inode 和 block
+ *        6. 可以对目录进行软连接
+ *        8. 删除链接文件的源文件，软连接文件无法指向了
+ *        9. 软连接可以跨目录
+ *
+ *
+ *      文件删除的原理：
+ *        文件删除的条件：
+ *            1. i_link = 0      硬链接文件和源文件都删除
+ *            2. i_count = 0     没有服务进程使用该文件
+ *
+ *        df -h   显示文件大小  会显示已经删除了的但是进程没有释放的文件的空间
+ *        du -sh  显示文件大小  不会显示已经删除但是还有程序调用的非静态文件 (显示文件占用的block大小)
+ *        ls -lh  显示文件大小  显示文件的实际大小  结果小于  du -sh
+ *        lsof  **********
+ *
+ *      4 工作中需要注意的地方
+ *     (1)当出现du和df差距很大的情况时，考虑是否是有删除文件未完成造成的，方法是lsof命令，然后停止相关进程即可。
+ *     (2)可以使用清空文件的方式来代替删除文件，方式是:echo > myfile.iso。
+ *     (3)对于经常发生删除问题的日志文件，以改名、清空、删除的顺序操作。
+ *     (4)除了rm外，有些命令会间接的删除文件，如gzip命令完成后会删除原来的文件，为了避免删除问题，压缩前先确认没有进程打开该文件。
+ *
+ *       磁盘满了的提示
+ *       no space left on device      代表磁盘满了后者inode满了
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
---
+++ test/centos6.5/200-300.php
@@ -0,0 +1,335 @@
+<?php
+/**
+ *    crontab 任务计划  ***************************未完待续
+ *      系统的任务计划表：
+ *        /etc/crontab
+ *      用户的任务计划表：
+ *        /var/spool/cron
+ *
+ *      特殊符号：
+ *      分 时 日 月 周
+ *      *               代表每天或者每周
+ *      -               17-19 代表一段范围
+ *      ，              17，19   代表17点和19点
+ *      /10             代表每十分中执行一次
+ *
+ *
+ *      crontab -l         查看当前用户的任务计划
+ *      crontab -e         修改当前用户的任务计划
+ *      crontab -r         删除当前用户的任务计划
+ *      crontab -u -l      查看其他用户的任务计划
+ *
+ *
+ *
+ *     用户管理：
+ *
+ *     /etc/skel 目录  (用户的环境变量目录)
+ *     1. 当我们使用useradd 添加一个用户的时候， 在添加的过程当中 相当于  cp /etc/skel  /home
+ *        复制这个目录到用户的家目录
+ *     2. 如果出现 bash-4.1 切换到出错用户 cp /etc/skel .
+ *
+ *     /etc/login.defs
+ *     1.文件是用来定义创建用户时需要的一些用户配置信息。如创建用户时是否需要家目录，密码有效期等等
+ *
+ *     /etc/defaults/useradd  (添加用户的时候用户的默认配置信息文件)
+ *     GROUP=100   依赖于/etc/login.defs 的USERGROUPS_ENAB参数，如果为no，则由此处控制
+ *     HOME=/home  用户的家目录创建在 /home中
+ *     INACTIVE=-1  是否启用账号过期停权，-1表示不启用
+ *     EXPIRE=      账号终止日期，不设置表示不启用
+ *     SHELL=/bin/bash    新用户默认使用的shell类型
+ *     SKEL=/etc/skel     配置新用户家目录默认的全局变量文件
+ *     CREATE_MAIL_SPOOL=yes    创建用户的mail文件
+ *
+ *     useradd
+ *     -c              新账号的passwd 档的说明栏。  在第五栏
+ *     -d              设置homedir(指定家目录)
+ *     -e              设置账号何时过期  格式 /MM/DD/YY
+ *     -g              指定账号属于的组  (主组)
+ *     -G              指定账号属于多个组  (附加组)
+ *     -m              创建家目录  (家目录可以不存在)
+ *     -M              不建立用户的家目录，优先于/etc/login.defs 文件中的设定
+ *     -s              指定用户的shell  如果默认不填写，会根据/etc/default/useradd 预设的值
+ *     -u              指定用户的id，id必须唯一，
+ *     -f              账户如果不修改密码何时停权，0为立刻停权，-1关闭此功能
+ *
+ *     usermod (修改用户信息) 和useradd一样
+ *     -l              变更用户login时候的login_name
+ *     -L              冻结用户密码  实质上就是修改/etc/shadow 在密码处加上！
+ *     -U              解除冻结   本质同上
+ *
+ *
+ *     groupadd
+ *     -g              指定组id
+ *
+ *     groupdel groupname     删除指定的用户组
+ *
+ *
+ *     passwd  (修改当前用户的密码)
+ *     passwd username (只有root可以修改别的用户的密码)
+ *     echo '123456' | passwd --stdin oldboy
+ *     -n 7                       7天以内不能修改密码
+ *     -x 60                      60天以后必须修改密码
+ *     -w 10                      还剩10天的时候提醒用户需要修改密码了
+ *     -i 10                      在密码过期后多少天，用户被禁掉，仅能root操作
+ *
+ *     修改用户密码属性的
+ *     chage -l username    查看用户的过期时间等设置
+ *     chage -E "2018/02/02" username   (相当于useradd -E 设置账户的有效期)
+ *
+ *     userdel 删除用户
+ *     -r                   删除用户的家目录
+ *
+ *
+ *      Password expires 的意思
+ *           chage 加M 选项有两个作用
+ *           一，Last passwrd change + M天数 = Password expires
+ *           二，Maximum number of days between password chang=M天数
+ *
+ *      password inactive  如果expires +  inactive  这个时间用户还是没有修改密码  则账号停用
+ *           此时只能联系管理员
+ *
+ *      http://www.mamicode.com/info-detail-1417735.html
+ *
+ *
+ *      用户查询相关命令：
+ *      id     用户组信息  id信息
+ *      w      用户登陆信息
+ *      who    显示谁登陆了   w命令包含who
+ *      last   显示登陆列表    /var/log/wtmp    查这个列表
+ *      lastlog      /ver/log/lastlog
+ *      groups     查询一个用户在哪个组里
+ *
+ *
+ *
+ *      su - huan0000 -c pwd   切换到一个用户执行一个命令再切换回来
+ *
+ *
+ *      sudo 命令
+ *      作用： 普通用户想要执行某些命令必须知道root密码 ， 但是通过sudo 可以在不知道
+ *      密码的时候可以授权成root用户
+ *
+ *      sudo 运行流程
+ *      运行sudo 命令  -》   检查 /var/db/sudo 下面的时间戳文件 (5分钟起效)  -》
+ *      输入密码(针对前面的检测)  =》  检查 /etc/sudoers 配置文件 用户可以执行的命令
+ *
+ *      visudo 命令 编辑 /etc/sudoers  文件
+ *      编辑98行：
+ *      huan0000 ALL=(ALL) NOPASSWD:/usr/sbin/useradd
+ *      %huan0000  ALL=(ALL) NOPASSWD:/usr/sbin/useradd    对一个用户组进行授权
+ *      用户名   可以执行的主机   可以执行的身份     可以执行的命令
+ *
+ *      编辑13行： 定义主机别名   把几个主机定义在一个别名上
+ *      Host_Alias FILESERVERS = fs1, fs2  (注意留3个空格)
+ *
+ *      编辑20行： 定义用户别名
+ *      User_Alias: ADMIN = user1, user2, %group1
+ *
+ *      Runas_Alias  定义用户身份别名
+ *      Runas_Alias some = root
+ *
+ *      编辑27行： 定义命令别名
+ *      Cmnd_Alias some = /sbin/route, /sbin/ifconfig
+ *
+ *
+ *      sudo -l      查看自己拥有哪些权限
+ *      sudo -v      查看自己是有可以使用sudo
+ *      sudo -u      指定用户的身份
+ *      sudo -k      删除时间戳  下回使用命令需要密码
+ *
+ *
+ *      sudo 配置文件注意事项：
+ *      ALL              必须都是大写
+ *      \                这个是换行的符号
+ *      ！/bin           可以是用！进行排除命令
+ *
+ *      远程sudo命令：
+ *      ssh -t hostname sudo       (-t强制远程执行sudo命令)
+ *      Defaults requiretty        (注释这行上面可以不用加-t就可以执行)
+ *
+ *      sudo审计：
+ *     1. 安装sudo  和  rsyslog 命令
+ *     2. echo "Defaults   logfile=/var/log/sudo.log" >> /etc/sudoers
+ *
+ *
+ *     255 --  263   回头再学
+ *
+ *     paste 合并文件
+ *     chpasswd   用户批量添加用户导入文件
+ *
+ *
+ *
+ *     264 -- 295   磁盘管理体系
+ *     磁盘的四种接口：
+ *       ide： 早期家用磁盘接口，逐渐被淘汰了
+ *       scsi：   早期服务器使用接口，逐渐被淘汰了
+ *       sata（sas）:    逐渐取代scsi接口的磁盘  通用sas串口   (sas 结合了scsi和sata的有点结合的升级版)
+ *       光纤：   高端服务器使用的接口磁盘
+ *      
+ *     磁盘读取数据原理：
+ *       先读一个磁道如果没有找到数据在读取同柱面的另一个磁道，如果还没有读取到则更改磁头
+ *       位置开始寻道继续读取，写入数据也是同理
+ *
+ *
+ *     分区：
+ *        分区信息记录在 0磁头 0磁道 1扇区中， 一共512字节
+ *        其中前446字节记录了引导纪录 mbr
+ *        剩余64字节纪录了分区表  (16个一组)
+ *         一块硬盘只可以分4个主分区(或者拓展分区)
+ *         一个主分区或者拓展分区的信息大于16kb
+ *
+ *        其中拓展分区可以再创建逻辑分区，逻辑分区的分区表和引导分区信息纪录在逻辑分区中。
+ *
+ *        16字节 分区表
+ *        1b   分区状态： 0 未激活    0x80  激活
+ *        1b   分区起始磁头号
+ *        2b   分区的起始扇区和柱面号
+ *        1b   分区类型
+ *        1b   分区结束磁头号
+ *        2b   分区结束扇区和柱面号
+ *        4b   线性寻址方式下分区相对扇区地址
+ *        4b   分区大小
+ *
+ *        分区工具 ：  fdisk，parted 分区工具分区的实质就是改变上述分区表的信息
+ *
+ *        dd if=/dev/sda  of=mbr.bin bs=512 count=1  (将一个磁盘的512字节做备份)
+ *        od -xa mbr.bin        (读取二进制文件)
+ *
+ *        fdisk 分区工具适合给小于2t的磁盘进行分区，parted可以给大于2t的磁盘进行分区
+ *        首选fdisk
+ *
+ *        1.磁盘分区是按照柱面来划分的 (从磁盘读写原理角度可以理解)
+ *        2.拓展分区不能直接使用，还要在拓展分区的基础上创建逻辑分区才可以
+ *        3.拓展分区有自己的分区表，因此拓展分区下面的逻辑分区可以有多个
+ *        4.一个硬盘只能有一个拓展分区，一个拓展分区可以划分成多个逻辑分区
+ *        5.逻辑分区的编号只能从5开始，即使只有一个主分区
+ *
+ *        scsi/sas/sata/usb 设备均以/dev/sd 开头。
+ *
+ *        fdisk工具的使用：
+ *        fdisk -l    查看分区状态
+ *
+ *        fidsk -cu /dev/sdb                          -c 切换到兼容模式  -u 显示的时候使用扇区(默认是柱面)
+ *        n                             添加分区
+ *        e / p / l                     添加主分区或者拓展分区或者逻辑分区
+ *        +10M                          设定分区大小
+ *        p                             查看分区
+ *        d                             删除一个分区
+ *        w                             保存
+ *        q                             退出不保存
+ *        l                             查看分区类型
+ *        t                             选择分区类型id (可以通过l查看)
+ *
+ *        partprobe / partx -a          通知内核分区表已经改变
+ *        ll /dev/sd*                   查看分区
+ *        cat /etc/proc/partitions      查看分区
+ *
+ *
+ *        parted 分区 (针对2t以上的硬盘)
+ *        1.   parted /dev/sdb
+ *        2.   mklabel gpt                      (将mbr改成gpt)
+ *        3.   mkpart primary 0 100             (分100m给一个主分区)
+ *        4.   p                                查看分好的分区
+ *        5.   mkpart primary linux-swap 11 20 Ignore     (分一个swap分区)
+ *        6.   mkpart logical ext4  21 30  Ignore         (不需要拓展分区分逻辑分区)
+ *        7.   p                                查看分区
+ *        8.   rm 1                             删除第一个分区
+ *
+ *        创建swap 分区
+ *        1. 创建一个普通分区
+ *        2. mkswap /dev/sdb1                创建一个swap分区
+ *        3. free -m                         查看swap分区
+ *        4. swapon /dev/sdb1                合并swap分区
+ *        5. swapoff /dev/sdb1               关掉其中的swap分区
+ *
+ *
+ *        linux 文件系统：
+ *        inode  存放一个文件的属性
+ *        block  存放一个文件的数据
+ *        superblock(metadate 元数据)   一个磁盘有多少个inode多少分区，多少block ，是superblock记录的。
+ *        dumpe2fs /dev/sdb             查看一个分区的元数据
+ *
+ *        inode 中包含如下信息，
+ *        文件的属主和属组。
+ *        文件的访问权限。
+ *        文件的类型
+ *        文件的访问，修改时间等。
+ *        文件的大小。
+ *        文件的各种标识，如suid ，sgid等
+ *        指向文件内容数据块的指针
+ *        一个inode大小通常是128字节，ext4文件系统的inode大小是256字节
+ *        inode表用于跟踪定位每个文件，包括位置，大小等。但是不包含文件名，一个块组只有一个inode表
+ *
+ *        什么是块设备：
+ *        块设备就是以块 (比如扇区) 为单位收发数据的设备。 他们支持缓冲和随机访问。
+ *        块设备包括硬盘，cd-rom等。
+ *
+ *        什么是字符设备：
+ *        字符设备可以进行物理寻址的媒体。 字符设备包括串行端口和磁带设备。只能逐字逐句的读取
+ *        这些设备中的数据。
+ *
+ *        什么是逻辑块：
+ *        逻辑块就是我们前面提到的block的概念。
+ *        硬盘的最小存储单位是扇区，而数据的最小存储单位是逻辑块
+ *
+ *
+ *        mkfs.ext4 -b 4096 -i 1024 /dev/sdb1        格式化成ext4文件系统
+ *        mkfs -t ext4 -b 4096 -i 1024 /dev/sdb1        格式化成ext4文件系统
+ *
+ *
+ *        ext2文件系统：
+ *        超级块      块组1          块组2
+ *        超级块      （组描述 块位示图    inode位势图   inode区   数据区）  属于快组1
+ *
+ *
+ *        dd                拷贝文件的一部分并指定格式
+ *        df -hiT           查看挂载分区的大小和文件系统  （h代表block，i代表inode，t是type）
+ *        cat /proc/mounts              查看挂载情况
+ *        cat /proc/partitions          查看分区情况
+ *        dumpe2fs  /dev/sdb1           查看文件系统的元数据
+ *        fsck  -C -f -t ext3 /dev/sdb1      检查磁盘损坏情况 (好的磁盘千万不能使用这个命令)
+ *        tune2fs                        修改文件系统信息
+ *        megacli                        查看raid信息
+ *        ipmitools                      查看硬件系统的信息
+ *        resize2fs                      调整文件系统的大小(lvm)
+ *
+ *        挂载：
+ *        mount -t         指定文件系统类型进行挂载
+ *        mount /dev/sdb1 /mnt      挂载ext2 和ext3 文件系统
+ *        mount -t iso9660 /cdrom /mnt       挂载光盘   (不加-t系统自己测试区挂载)
+ *
+ *        mkfs -t vfat /dev/fd0            格式化软盘
+ *        mount -t fat /dev/fd0 /media/floop    挂载软盘
+ *
+ *        mount -t vfat -o iocharset=cp950 /dev/sdb1 /tmp/flash   挂载U盘
+ *
+ *        umount  /dev/sdb1    解除挂载
+ *        umount -lF           强制卸载
+ *
+ *
+ *        开机自动挂载：
+ *        vim /etc/fstab
+ *        /dev/sdb2   /tmp    ext3    defaults   0(是否备份)    0(是否开机做磁盘检查)
+ *
+ *
+ *        split，paste，sort，wc, dos2unix ,diff       等命令详解
+ *
+ *        tr命令：
+ *        tr 'abc' 'ABC' < ./text           替换单个字符
+ *        tr -d 0 < text                    删除文件中所有的0
+ *        tr -d -c '0-9' > text             删除所有除了0-9以外的字符
+ *
+ *        tee命令：
+ *        将结果输出到屏幕的同时输入到文件
+ *        ls | tee /tmp/test.txt
+ *        ls | tee -a /tmp/test.txt             追加写入一个文件之中
+ *
+ *        
+ *
+ *
+ *
+ *
+ */
+
+
+
+
